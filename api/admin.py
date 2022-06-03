28fd8a9c4d34e170bcac63fe2c095015c72ebb8c
zoegoodwinsutton <zoe.goodwinsutton@gmail.com>
2022-06-03 12:47:43 -0700

    fixed most linting errors

--- api/admin.py
+++ api/admin.py
@@ -3,32 +3,45 @@ admin_bp = Blueprint('admin', __name__)
 
 @admin_bp.route('/hello/')
 def hello():
+    '''
+    says hello
+    '''
     return "Hello from Admins", 200
-admins = [
-        {'uuid': 'd86a6640-d267-42f0-9dcd-8c0d06bf884c', 'first_name':'Rich', 'last_name':'Little', 'email':'richlittle@uvic.ca'},
-        {'uuid': 'ace7c38c-a6dd-467a-b73f-aaaafe9a38b2', 'first_name':'Dan', 'last_name': 'Mai', 'email':'danmai@uvic.ca'}
+ADMINS = [
+        {'uuid': 'd86a6640-d267-42f0-9dcd-8c0d06bf884c', 'first_name':'Rich', 'last_name':'Little',
+         'email':'richlittle@uvic.ca'},
+        {'uuid': 'ace7c38c-a6dd-467a-b73f-aaaafe9a38b2', 'first_name':'Dan', 'last_name': 'Mai',
+         'email':'danmai@uvic.ca'}
     ]
-uuids = [admin['uuid'] for admin in admins]
-# returns all admins and their URIs
+UUIDS = [admin['uuid'] for admin in ADMINS]
+
 @admin_bp.route('/', methods=['GET'])
 def get_all_admins():
-    return jsonify(admins), 200
-    
-# returns a specific professor's account information
-@admin_bp.route('/<id>', methods=['GET'])
-def get_admin(id):
-    print(id)
-    if id not in uuids:
+    '''
+    returns all ADMINS and their URIs
+    '''
+    return jsonify(ADMINS), 200
+
+@admin_bp.route('/<admin_id>', methods=['GET'])
+def get_admin(admin_id):
+    '''
+    returns a specific professor's account information
+    '''
+    if admin_id not in UUIDS:
         return 'id not valid', 404
     else:
-        return jsonify(admins[uuids.index(id)]), 200
+        return jsonify(ADMINS[UUIDS.index(admin_id)]), 200
 
-# posts a new professor
 @admin_bp.route('/', methods=['POST'])
 def post_admin():
-    return 'added user ',200
+    '''
+    posts a new admin
+    '''
+    return 'added user ', 200
 
-# deletes an admin from the admin table
-@admin_bp.route('/<id>', methods=['DELETE'])
-def delete_admin(id):
-    return f'deleted user with id {id}',200
+@admin_bp.route('/<admin_id>', methods=['DELETE'])
+def delete_admin(admin_id):
+    '''
+    deletes an admin from the admin table
+    '''
+    return f'deleted user with id {admin_id}', 200
--- api/professor.py
+++ api/professor.py
@@ -1,63 +1,95 @@
+'''
+contains all API /professors endpoints
+'''
 from flask import Blueprint, jsonify
 professor_bp = Blueprint('professor', __name__)
 @professor_bp.route('/hello/')
 def hello():
+    '''
+    blah
+    '''
     return "Hello from Professors"
-professors = [
-    {'first_name':'Celina', 'last_name': 'Berg', 'uuid':'0e90ab30-c380-4034-acdb-238856a88df3', 'department':'CSC', 'is_teaching':True,'email':'email@uvic.ca' },
-    {'first_name':'Bill', 'last_name': 'Bird', 'uuid': '8b8829ec-4615-4708-a0cd-5103f080ae56','department':'CSC', 'is_teaching':True,'email':'email@uvic.ca' },
-    {'first_name':'Anthony', 'last_name': 'Estey', 'uuid':'6e46c60b-6709-4af5-ab4e-7a1c89c8ae0b','department':'CSC', 'is_teaching':True,'email':'email@uvic.ca' },
+PROFESSORS = [
+    {'first_name':'Celina', 'last_name':'Berg', 'uuid':'0e90ab30-c380-4034-acdb-238856a88df3',
+     'department':'CSC', 'is_teaching':True, 'email':'email@uvic.ca'},
+    {'first_name':'Bill', 'last_name':'Bird', 'uuid':'8b8829ec-4615-4708-a0cd-5103f080ae56',
+     'department':'CSC', 'is_teaching':True, 'email':'email@uvic.ca'},
+    {'first_name':'Anthony', 'last_name':'Estey', 'uuid':'6e46c60b-6709-4af5-ab4e-7a1c89c8ae0b',
+     'department':'CSC', 'is_teaching':True, 'email':'email@uvic.ca'},
 ]
-reliefs = [
-    {'prof_id':'0e90ab30-c380-4034-acdb-238856a88df3', 'id':1, 'year':2022, 'num_relief': 2, 'num_summer_courses':1, 'num_fall_courses':1, 'num_spring_courses':2},
-    {'prof_id':'8b8829ec-4615-4708-a0cd-5103f080ae56', 'id':2, 'year':2022, 'num_relief': 2, 'num_summer_courses':1, 'num_fall_courses':1, 'num_spring_courses':2},
-    {'prof_id':'6e46c60b-6709-4af5-ab4e-7a1c89c8ae0b', 'id':3, 'year':2022, 'num_relief': 2, 'num_summer_courses':1, 'num_fall_courses':1, 'num_spring_courses':2},
+RELIEFS = [
+    {'prof_id':'0e90ab30-c380-4034-acdb-238856a88df3', 'id':1, 'year':2022, 'num_relief': 2,
+     'num_summer_courses':1, 'num_fall_courses':1, 'num_spring_courses':2},
+    {'prof_id':'8b8829ec-4615-4708-a0cd-5103f080ae56', 'id':2, 'year':2022, 'num_relief': 2,
+     'num_summer_courses':1, 'num_fall_courses':1, 'num_spring_courses':2},
+    {'prof_id':'6e46c60b-6709-4af5-ab4e-7a1c89c8ae0b', 'id':3, 'year':2022, 'num_relief': 2,
+     'num_summer_courses':1, 'num_fall_courses':1, 'num_spring_courses':2},
 ]
-uuids = [prof['uuid'] for prof in professors]
-# returns all professors
+UUIDS = [prof['uuid'] for prof in PROFESSORS]
+
 @professor_bp.route('/', methods=['GET'])
 def get_all_professors():
-    return jsonify(professors),200
+    '''
+    returns all professors
+    '''
+    return jsonify(PROFESSORS), 200
 
-#returns a professor with an ID
-@professor_bp.route('/<id>', methods=['GET'])
-def get_professor(id):
-    return jsonify(professors[uuids.index(id)]),200
+@professor_bp.route('/<professor_id>', methods=['GET'])
+def get_professor(professor_id):
+    '''
+    returns a professor with an ID
+    '''
+    return jsonify(PROFESSORS[UUIDS.index(professor_id)]), 200
 
-#returns all of professor's preferences
-@professor_bp.route('/<id>/preferences', methods=['GET'])
-def get_professor_preferences(id):
-    return jsonify(reliefs[uuids.index(id)]),200
+@professor_bp.route('/<professor_id>/preferences', methods=['GET'])
+def get_professor_preferences(professor_id):
+    '''
+    returns all of professor's preferences
+    '''
+    return jsonify(RELIEFS[UUIDS.index(professor_id)]), 200
 
-#returns a professor's preferences for a certain year
-@professor_bp.route('/<id>/preferences/<preference_id>', methods=['GET'])
-def get_professor_preference(id, preference_id):
-    if reliefs[uuids.index(id)]['id'] != int(preference_id):
+@professor_bp.route('/<professor_id>/preferences/<preference_id>', methods=['GET'])
+def get_professor_preference(professor_id, preference_id):
+    '''
+    returns a professor's preferences for a certain year
+    '''
+    if RELIEFS[UUIDS.index(professor_id)]['id'] != int(preference_id):
         return 'couldn\'t find that preference id', 404
     else:
-        return jsonify(reliefs[uuids.index(id)]),200
+        return jsonify(RELIEFS[UUIDS.index(professor_id)]), 200
 
-# adds a new professor
 @professor_bp.route('/', methods=['POST'])
 def post_professor():
-    return 'posted a professor',200
+    '''
+    adds a new professor
+    '''
+    return 'posted a professor', 200
 
-# adds a new professor's preferences
-@professor_bp.route('/<id>/preferences', methods=['POST'])
-def post_professor_preferences(id):
-    return f'updates a prof with id {id}',200
+@professor_bp.route('/<professor_id>/preferences', methods=['POST'])
+def post_professor_preferences(professor_id):
+    '''
+    adds a new professor's preferences
+    '''
+    return f'updates a prof with professor_id {professor_id}', 200
 
-# updates a professor's preferences
-@professor_bp.route('/<id>/preferences/<preference_id>', methods=['PUT'])
-def update_professor_preferences(id, preference_id):
-    return f'updates the preferences with id {preference_id} for professor with id {id}',200
+@professor_bp.route('/<professor_id>/preferences/<preference_id>', methods=['PUT'])
+def update_professor_preferences(professor_id, preference_id):
+    '''
+    updates a professor's preferences
+    '''
+    return f'updates the preferences with id {preference_id} for \
+     professor with id {professor_id}', 200
 
-# deletes a professor
-@professor_bp.route('/<id>', methods=['DELETE'])
-def delete_professor(id):
-    return f'deleted prof with id {id}',200
+@professor_bp.route('/<professor_id>', methods=['DELETE'])
+def delete_professor(professor_id):
+    '''
+    deletes a professor
+    '''
+    return f'deleted prof with id {professor_id}', 200
 
-# deletes a professor's preferences
-@professor_bp.route('/<id>/preferences/<preference_id>', methods=['DELETE'])
-def delete_professor_preferences(id, preference_id):
-    return f'deleted preference with id {preference_id} for professor with id {id}',200
+@professor_bp.route('/<professor_id>/preferences/<preference_id>', methods=['DELETE'])
+def delete_professor_preferences(professor_id, preference_id):
+    '''
+    deletes a professor's preferences
+    '''
+    return f'deleted preference with id {preference_id} for professor with id {professor_id}', 200
