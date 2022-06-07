61f436bc7686ca27daff1876c0457fd0e61cf897
Annie <annie_zhou@live.ca>
2022-06-07 11:02:12 -0700

    Update database schema
    - change schedule table to use JSON datatype
    - add algo 2 output table
    - add preferred_times column to prof course pref table
    - rename prof course pref table to prof availability table
    - add why_relief to relief table
    - change able_to_teach column from boolean type to string
    - add is_peng column to prof table
    - add historical data table

--- database/sql/schema.sql
+++ database/sql/schema.sql
@@ -14,37 +14,54 @@ CREATE TABLE Professor (
 	last_name VARCHAR(30),
 	email VARCHAR(60) NOT NULL,
 	department VARCHAR(30) NOT NULL,
-	is_teaching BOOLEAN NOT NULL
+	is_teaching BOOLEAN NOT NULL,
+	is_peng BOOLEAN NOT NULL
 );
 
 CREATE TABLE Schedule (
 	id BINARY(16) PRIMARY KEY,
-	schedule TEXT(50000) NOT NULL,
+	result JSON NOT NULL,
 	semester CHAR NOT NULL,
 	year int NOT NULL
+);
+
+CREATE TABLE Algo2Output (
+	id BINARY(16) PRIMARY KEY,
+	result JSON NOT NULL,
+	semester CHAR NOT NULL,
+	year int NOT NULL
+);
+
+CREATE TABLE HistoricalData(
+	id BINARY(16) PRIMARY KEY,
+	result JSON NOT NULL
 );
 
 CREATE TABLE CourseOffering (
 	id BINARY(16) PRIMARY KEY,
 	course_name VARCHAR(30) NOT NULL,
 	course_code VARCHAR(10) NOT NULL, 
+	course_desc VARCHAR(500) NOT NULL,
+	prof_prereq VARCHAR(500),
 	min_offering INT NOT NULL,
-	spring_required BOOLEAN NOT NULL,
-	summer_required BOOLEAN NOT NULL,
-	fall_required BOOLEAN NOT NULL,
+	spring_req BOOLEAN NOT NULL,
+	summer_req BOOLEAN NOT NULL,
+	fall_req BOOLEAN NOT NULL,
 	spring_peng_req BOOLEAN NOT NULL,
 	summer_peng_req BOOLEAN NOT NULL,
 	fall_peng_req BOOLEAN NOT NULL
 );
 
-CREATE TABLE ProfessorRelief (
+CREATE TABLE ProfessorAvailability (
 	id  BINARY(16) PRIMARY KEY,
 	prof_id  BINARY(16) NOT NULL,
 	year INT NOT NULL,
 	num_relief INT NOT NULL,
+	why_relief VARCHAR(100),
 	num_summer_courses INT NOT NULL,
 	num_fall_courses INT NOT NULL,
 	num_spring_courses INT NOT NULL,
+	preferred_times JSON NOT NULL,
 	FOREIGN KEY(prof_id)
 		REFERENCES Professor(id)
 		ON DELETE CASCADE
@@ -53,18 +70,18 @@ CREATE TABLE ProfessorRelief (
 
 CREATE TABLE ProfessorCoursePreference (
 	course_id BINARY(16) NOT NULL,
-	prof_relief_id BINARY(16) NOT NULL,
+	prof_avail_id BINARY(16) NOT NULL,
 	year INT NOT NULL,
 	will_to_teach VARCHAR(12) NOT NULL,
-	able_to_teach BOOLEAN NOT NULL,
+	able_to_teach VARCHAR(12) NOT NULL,
 	time_stamp DATETIME NOT NULL,
-	FOREIGN KEY(prof_relief_id)
-		REFERENCES ProfessorRelief(id)
+	FOREIGN KEY(prof_avail_id)
+		REFERENCES ProfessorAvailability(id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
 	FOREIGN KEY(course_id) 
 		REFERENCES CourseOffering(id)
 		On DELETE CASCADE
 		ON UPDATE CASCADE,
-	PRIMARY KEY(course_id, prof_relief_id)
+	PRIMARY KEY(course_id, prof_avail_id)
 );
