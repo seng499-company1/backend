d162778ac5fb9cb06e95a7d09095d72c8d83cf5f
Owen Jaques <owenjaques2000@gmail.com>
2022-06-09 15:10:51 -0700

    fixed all pylint errors

--- database/demo/login.py
+++ database/demo/login.py
@@ -1,8 +1,16 @@
+"""
+Simple login demo to demonstrate loging into the database from Python.
+"""
+
 import mysql.connector
 
-conn = mysql.connector.connect(user='api_user',
-	password='password', 
-	host='127.0.0.1',
-	database='scheduler')
+DB_CONN = mysql.connector.connect(user='api_user',
+                                  password='password',
+                                  host='127.0.0.1',
+                                  database='scheduler')
 
-conn.close()
+CURSOR = DB_CONN.cursor()
+
+CURSOR.execute("SHOW TABLES;")
+
+DB_CONN.close()
