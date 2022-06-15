19d9d97cd57a6cbd6011332ea98b6ae0ca86c75f
Emmanuel Ekanem <ekyeinstein2012@gmail.com>
2022-06-15 15:50:17 -0700

    Update wait-for-db.sh

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,4 +1,4 @@
-while ! docker exec backend_db touch /tmp/execWorks >/dev/null 2>&1; 
+while ! docker exec "backend_db" mysql --user="root" --password="root" -e "SELECT 1" >/dev/null 2>&1; 
 do
     sleep 1
 done
