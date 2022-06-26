dbb719d290886f8df6a7281f854c50ba3329825c
Owen Jaques <owenjaques2000@gmail.com>
2022-06-25 23:18:33 -0700

    add courses to db automatically

--- database/sql/z_init.sql
+++ database/sql/z_init.sql
@@ -7,3 +7,10 @@ LOAD DATA INFILE '/csv/profs.csv'
 	IGNORE 1 LINES
 	(first_name,last_name,email,department,is_teaching,is_peng)
 	SET id = UUID_TO_BIN(UUID());
+
+LOAD DATA INFILE '/csv/courses.csv' 
+	INTO TABLE CourseOffering
+	FIELDS TERMINATED BY ','
+	IGNORE 1 LINES
+	(course_name,course_code,course_desc,prof_prereq,min_offering,spring_req,summer_req,fall_req,spring_peng_req,summer_peng_req,fall_peng_req)
+	SET id = UUID_TO_BIN(UUID());
