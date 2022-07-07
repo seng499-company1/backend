48515918303184401e8bef546d22fde8d6cf20f0
Owen Jaques <owenjaques2000@gmail.com>
2022-07-07 00:47:54 -0700

    Added sample admin to seed data for testing.

--- database/sql/z_init.sql
+++ database/sql/z_init.sql
@@ -13,3 +13,10 @@ LOAD DATA INFILE '/init_data/courses.txt'
 	IGNORE 1 LINES
 	(course_name,course_code,course_desc,prof_prereq,min_offering,spring_req,summer_req,fall_req,spring_peng_req,summer_peng_req,fall_peng_req,year_req,notes)
 	SET id = UUID_TO_BIN(UUID());
+
+LOAD DATA INFILE '/init_data/admin.txt' 
+	INTO TABLE Admin
+	FIELDS TERMINATED BY '\t'
+	IGNORE 1 LINES
+	(first_name, last_name, email)
+	SET id = UUID_TO_BIN(UUID());
--- demos/sprint3/demo.sh
+++ demos/sprint3/demo.sh
@@ -1,4 +1,19 @@
-# succesfully pings the login endpoint for a professor
+echo 'admin login'
+curl -H "Content-Type: application/json" \
+    -d '{"username":"admin", "password":"password"}' \
+    http://localhost:5000/login/
+
+echo 'professor login'
 curl -H "Content-Type: application/json" \
     -d '{"username":"mdadams", "password":"password"}' \
+    http://localhost:5000/login/
+
+echo 'wrong password'
+curl -H "Content-Type: application/json" \
+    -d '{"username":"mdadams", "password":"pasord"}' \
+    http://localhost:5000/login/
+
+echo 'invalid user'
+curl -H "Content-Type: application/json" \
+    -d '{"username":"mdas", "password":"pasord"}' \
     http://localhost:5000/login/
