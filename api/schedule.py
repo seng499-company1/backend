012c2e42d009b43960feb7e5d639f32e648bc3d2
zoegoodwinsutton <zoe.goodwinsutton@gmail.com>
2022-07-19 12:02:55 -0700

    updated both algo packages and removed bug fix code

--- api/schedule.py
+++ api/schedule.py
@@ -81,8 +81,6 @@ def get_company_schedule(company_num):
 
     if company_num == '1':
         schedule = c1alg2(historical_data, previous_enrolment, schedule)
-        # removes extra instance of SENG275 that Algo2 is adding
-        schedule['fall'].pop()
         final_schedule, _ = c1alg1.generate_schedule(professors, schedule)
     elif company_num == '2':
         schedule = c2alg2(historical_data, previous_enrolment, schedule)
