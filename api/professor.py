4c540bfd2fd2a0a80eaf48c0e46a1ceabb947dab
Emmanuel Ekanem <ekyeinstein2012@gmail.com>
2022-07-22 01:15:23 -0700

    Update professor.py

--- api/professor.py
+++ api/professor.py
@@ -50,8 +50,8 @@ def get_all_professors():
     if isinstance(results, str):
         return results, 400
 
-    with open('populate_prof_prefs/curr_professors.json', 'w', encoding='utf-8') as file_handle:
-        json.dump(results.json,file_handle)
+#     with open('populate_prof_prefs/curr_professors.json', 'w', encoding='utf-8') as file_handle:
+#         json.dump(results.json,file_handle)
     return results, 200
 
 @PROFESSOR_BP.route('/', methods=['POST'])
