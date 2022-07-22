055a1286fc13011bc7ca8241a21d82e6c0d4f9e0
zoegoodwinsutton <zoe.goodwinsutton@gmail.com>
2022-07-22 16:11:22 -0700

    added static courses

--- api/schedule.py
+++ api/schedule.py
@@ -77,12 +77,11 @@ def get_company_schedule(company_num):
     schedule = get_empty_schedule()
     previous_enrolment = get_previous_enrolment()
     historical_data = get_historical_data()
-    
     if company_num == '1':
         schedule = c1alg2(historical_data, previous_enrolment, schedule)
         final_schedule, _ = c1alg1.generate_schedule(professors, schedule)
         print(final_schedule)
-        return None, 200
+        # return None, 200
     elif company_num == '2':
         schedule = c2alg2(historical_data, previous_enrolment, schedule)
         final_schedule, _ = c2alg1(professors, schedule)
